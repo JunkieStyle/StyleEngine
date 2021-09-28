29a0336807a6c665a90acaeb2246389e1db0a8a1
Anton Nekhaev <antonnekhaev@gmail.com>
2021-09-29 02:32:50 +0300

    added buildsln support for linux and macos

--- tools/buildsln.py
+++ tools/buildsln.py
@@ -2,5 +2,13 @@ import subprocess
 
 import globals
 
+CONFIG = "DEBUG"
+
 if globals.is_windows():
     subprocess.call(["cmd.exe", "/c", globals.VS_BUILD_PATH, f"{globals.ENGINE_NAME}.sln"])
+
+if globals.is_linux():
+    subprocess.call(["make", f"config={CONFIG}"])
+
+if globals.is_macos():
+    subprocess.call(["make", f"config={CONFIG}"])
