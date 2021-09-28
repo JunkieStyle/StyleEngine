5e93644f01ed64605088646592ae7b60e44c555e
Anton Nekhaev <antonnekhaev@gmail.com>
2021-09-29 02:36:02 +0300

    prettyfied cli.py

--- cli.py
+++ cli.py
@@ -6,8 +6,6 @@ parser = argparse.ArgumentParser(description="Style Engine CLI tools launcher")
 parser.add_argument("commands", nargs="+", help="command name")
 args = parser.parse_args()
 
-print("\n" + "-" * 80)
-
 
 def run_command(command, command_executable="python3"):
     command_path = os.path.join(os.getcwd(), "tools", f"{command}.py")
@@ -15,9 +13,10 @@ def run_command(command, command_executable="python3"):
         print(f"Invalid command: {command}")
         return
 
-    print(f"Executing command: {command}")
     subprocess.call([command_executable, command_path])
 
 
 for command in args.commands:
+    print("\n" + "-" * 80)
+    print(f"Executing command: {command}")
     run_command(command)
